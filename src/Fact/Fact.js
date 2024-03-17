a4f90b793786d88da97dd58a2d12b4e1e67ddea6
Валерия Тян <vlrtyan@Valerias-MacBook-Air.local>
2024-03-17 21:50:39 +0300

    fix Fact

--- src/Fact/Fact.js
+++ src/Fact/Fact.js
@@ -1,24 +1,39 @@
 import React from "react";
 import "./Fact.css";
-import {
-  Button
-} from "@vkontakte/vkui";
+import { Button } from "@vkontakte/vkui";
 
 function Fact() {
   const [result, setResult] = React.useState("");
-  const getFact = () => {
-    fetch("https://catfact.ninja/fact")
-      .then((res) => res.json())
-      .then((json) => setResult(json.fact))
-      .catch((err) => setResult("Something went wrong"));
+  const resRef = React.useRef(null);
+
+  const setCursor = (element, position) => {
+    if (element.setSelectionRange) {
+      element.focus();
+      setTimeout(() => {element.setSelectionRange(position, position)}, 0);
+    }
+  };
+
+  const getFact = async () => {
+    try {
+      const res = await fetch("https://catfact.ninja/fact");
+      const json = await res.json();
+      setResult(json.fact);
+      if (json.fact) {
+        const firstSpaceIndex = json.fact.search(/\W/);
+        console.log(firstSpaceIndex)
+        if (firstSpaceIndex != - 1) {
+          setCursor(resRef.current, firstSpaceIndex);
+        }
+      }
+    } catch (err) {
+      setResult("Something went wrong");
+    }
   };
 
   return (
     <section className="fact">
-      <Button onClick={getFact}>
-        Get a fact
-      </Button>
-      <p className="fact_text">{result}</p>
+      <Button onClick={getFact}>Get a fact</Button>
+      <textarea className="fact_text" defaultValue={result} ref={resRef} rows="5" cols="50"></textarea>
     </section>
   );
 }
